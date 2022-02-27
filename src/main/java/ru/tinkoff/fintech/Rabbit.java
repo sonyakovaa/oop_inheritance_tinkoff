a38b090b7bab244131c9534664c510d2081cadbd
Sofya Kovaleva <s.kovaleva@profsoft.pro>
2022-02-27 18:11:55 +0400

    Добавлено решение задания 4.4 и комментарий к конструкторам животных.

--- src/main/java/ru/tinkoff/fintech/Rabbit.java
+++ src/main/java/ru/tinkoff/fintech/Rabbit.java
@@ -10,7 +10,7 @@ public class Rabbit extends Animal {
         } else {
             this.setName("Rabbit without name");
         }
-        /* В ветке else вызов System.exit(1) ломает тесты, поэтому добавила создание экземпляра животного без имени.
-         * Если же не добавить, то он будет создавать экземпляр животного с именем null. */
+        /* In the else branch, the System.exit(1) call breaks the tests, so I added the creation of an instance of an animal without a name.
+         * If not added, it will create an instance of an animal named null. */
     }
 }
